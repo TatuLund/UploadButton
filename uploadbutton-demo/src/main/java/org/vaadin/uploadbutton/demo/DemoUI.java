b351623d087c4285f0c04a8461d8d2ccbc23950d
Tatu Lund <tatu@vaadin.com>
2018-10-15 20:14:12 +0300

    Improved demo

--- uploadbutton-demo/src/main/java/org/vaadin/uploadbutton/demo/DemoUI.java
+++ uploadbutton-demo/src/main/java/org/vaadin/uploadbutton/demo/DemoUI.java
@@ -1,5 +1,9 @@
 package org.vaadin.uploadbutton.demo;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
 import javax.servlet.annotation.WebServlet;
 
 import org.vaadin.uploadbutton.UploadButton;
@@ -13,30 +17,71 @@ import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.UI;
+import com.vaadin.ui.Upload.FailedEvent;
+import com.vaadin.ui.Upload.FailedListener;
+import com.vaadin.ui.Upload.ProgressListener;
+import com.vaadin.ui.Upload.Receiver;
+import com.vaadin.ui.Upload.StartedEvent;
+import com.vaadin.ui.Upload.StartedListener;
+import com.vaadin.ui.Upload.SucceededEvent;
+import com.vaadin.ui.Upload.SucceededListener;
 import com.vaadin.ui.VerticalLayout;
 
 @Theme("demo")
 @Title("UploadButton Add-on Demo")
 @SuppressWarnings("serial")
-public class DemoUI extends UI
-{
+public class DemoUI extends UI implements SucceededListener, FailedListener, ProgressListener {
 
     @WebServlet(value = "/*", asyncSupported = true)
     @VaadinServletConfiguration(productionMode = false, ui = DemoUI.class)
     public static class Servlet extends VaadinServlet {
     }
 
+    // Dummy receiver
+    public class Uploader implements Receiver {
+
+    	@Override
+    	public OutputStream receiveUpload(String filename,
+                                       String mimeType) {
+    		System.out.println(filename+" "+mimeType);
+         	return new ByteArrayOutputStream(10240);
+     	}
+
+    };
+
+	@Override
+ 	public void uploadSucceeded(SucceededEvent event) {
+ 		System.out.println(event.getFilename()+" "+event.getLength()+" bytes");
+ 	}
+
+	@Override
+	public void updateProgress(long readBytes, long contentLength) {
+		System.out.println("Status: "+readBytes+"/"+contentLength);
+		
+	}
+
+	@Override
+	public void uploadFailed(FailedEvent event) {
+		System.out.println("Failed");
+	}
+
     @Override
     protected void init(VaadinRequest request) {
 
-        UploadButton upload1 = new UploadButton("My Upload",
-                (filename, mimeType)-> { return null; });
-        upload1.setIcon(VaadinIcons.UPLOAD);
+    	Uploader receiver = new Uploader();
 
-        UploadButton upload2 = new UploadButton("My Upload",
-                (filename, mimeType)-> { return null; });
+        UploadButton upload1 = new UploadButton("My Upload", receiver);
+        upload1.setIcon(VaadinIcons.UPLOAD);
+        upload1.addProgressListener(this);
+        upload1.addSucceededListener(this);
+        upload1.addFailedListener(this);
+        
+        UploadButton upload2 = new UploadButton("My Upload", receiver);
         upload2.setCaptionAsHtml(true);
         upload2.setButtonCaption(VaadinIcons.UPLOAD.getHtml()+" My Upload");
+        upload2.addProgressListener(this);
+        upload2.addSucceededListener(this);
+        upload2.addFailedListener(this);
         
         Button button = new Button("Fake Upload");
         button.setIcon(VaadinIcons.UPLOAD_ALT);
